3eb11a0e5c44ad1fdd55da7a9dac86cf4f2b4928
Victoria Beregova <gerzhan6@gmail.com>
2016-08-08 16:56:19 +0300

    Модуль 1 задание 2

--- build/js/check.js
+++ build/js/check.js
@@ -1,57 +1,29 @@
-/*
- 3. В файле check.js создайте пустую функцию getMessage.
- Эта функция должна будет принимать на вход два параметра,
- a и b, анализировать их и возвращать сообщение-строку.
-
- Как должна работать функция?
-
- Если первый аргумент, a, имеет тип boolean, то:
- Если он true, вернуть строку, в которую подставлен параметр b:
- "Я попал в [b]"
- Если он false, то вернуть строку:
- "Я никуда не попал"
-
- Если первый аргумент имеет числовой тип, то вернуть строку:
- "Я прыгнул на [a] * 100 сантиметров"
-
- Если первый аргумент массив, то вернуть строку:
- "Я прошёл [numberOfSteps] шагов"
- где [numberOfSteps] — это сумма значений переданного массива
-
- Если оба аргумента массивы, то вернуть строку:
- "Я прошёл [distancePath] метров"
- где [distancePath] — это сумма произведений соответствующих
- элементов массивов a и b,
- cумма произведения первого элемента a с первым элементом b,
- второго со вторым и так далее
- */
+'use strict';
 function getMessage(a, b) {
     if (typeof a === 'boolean') {
         if (a) {
-            return 'Я попал в [b]';
+            return 'Я попал в ' + b + '';
         } else {
             return 'Я никуда не попал';
         }
+
+        function getArrayMultiplicatingSum(array){
+          var distancePath = 0;
+          for (var i = 0; i < array.length; i++) {
+              distancePath += a[i] * b[i];
+          }
+
+          function getArrayMultiplicatingSum(array){
+            var numberOfSteps = 0;
+            for (var i = 0; i < array.length; i++) {
+                numberOfSteps += a[i];
+
     } else if (typeof a === 'number') {
-        return 'Я прыгнул на [' + a + '] * 100 сантиметров: ' + (a * 100);
+        return 'Я прыгнул на  ' + a + ' * 100 сантиметров';
     } else if (Array.isArray(a) && Array.isArray(b)) {
-        var distancePath = 0;
-        for (var i = 0; i < a.length; i++) {
-            distancePath += a[i] * b[i];
-        }
-        return 'Я прошёл [' + distancePath + '] метров';
+       'Я прошёл ' + arraySum(a, b) + ' метров';
 
     } else if (Array.isArray(a)) {
-        var numberOfSteps = 0;
-        for (var i = 0; i < a.length; i++) {
-            numberOfSteps += a[i];
-        }
-        return 'Я прошёл [' + numberOfSteps + '] метров';
+       'Я прошёл ' + arraySum(a) + ' метров';
     }
 }
-
-console.log(getMessage(true));
-console.log(getMessage(false));
-console.log(getMessage(12));
-console.log(getMessage([1, 2, 3, 4, 5]));
-console.log(getMessage([2, 4, 5], [1, 2, 3]));
