c47ea5b164ede0c95f25152de1d68946ec5d65d8
Victoria Beregova <gerzhan6@gmail.com>
2016-08-12 16:30:18 +0300

    Модуль 1 задание 2

--- build/js/check.js
+++ build/js/check.js
@@ -2,27 +2,35 @@
 
 function getMessage(a, b) {
     if (typeof a === 'boolean') {
-        if (a) {
-            return 'Я попал в ' + b + '';
-        } else {
-            return 'Я никуда не попал';
-        }else if (typeof a === 'number') {
-            return 'Я прыгнул на  ' + a + ' * 100 сантиметров';
-        }
-};
-        function getArray1MultiplicatingSum(array){
-          var distancePath = 0;
-          for (var i = 0; i < array.length; i++) {
-              distancePath += a[i] * b[i];
-          }if (Array.isArray(a) && Array.isArray(b)) {
-             'Я прошёл ' + arraySum(a, b) + ' метров';
-           }
-};
-          function getArray2MultiplicatingSum(array){
-            var numberOfSteps = 0;
-            for (var i = 0; i < array.length; i++) {
-                numberOfSteps += a[i];
-              }if (Array.isArray(a)) {
-                 'Я прошёл ' + arraySum(a) + ' метров';
+    return 'Я попал в ' + b + '';
+   }else {
+    return 'Я никуда не попал';
+   }else if (typeof a === 'number') {
+     return 'Я прыгнул на  ' + a + ' * 100 сантиметров';
+   }if (Array.isArray(a) && Array.isArray(b)) {
+         'Я прошёл ' + getArrayMultiplicatingSum(a, b) + ' метров';
+    }if (Array.isArray(a)) {
+         'Я прошёл ' + getArraySum(a) + ' метров';
               }
-  };
+    };
+
+    function getArraySum(array){
+      var sum = 0;
+      for(var i = 0; i < array.length; i++){
+      sum += array[i];
+      }
+      console.log(sum);
+      }
+      getArraySum(arr);
+       };
+
+
+  function getArrayMultiplicatingSum(a, b){
+      var sum = 0;
+      for (var i = 0; i < array.length; i++)
+      sum += a[i] * b[i];
+      }
+      console.log(sum);
+      }
+      getArrayMultiplicatingSum(arr);
+      };
