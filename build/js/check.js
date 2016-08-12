e4f3ae95fd3bcb64a63c44b9d18a807c058ed854
Victoria Beregova <gerzhan6@gmail.com>
2016-08-12 10:55:20 +0300

    Модуль 1 задание 2

--- build/js/check.js
+++ build/js/check.js
@@ -1,31 +1,28 @@
 'use strict';
+
 function getMessage(a, b) {
     if (typeof a === 'boolean') {
         if (a) {
             return 'Я попал в ' + b + '';
         } else {
             return 'Я никуда не попал';
+        }else if (typeof a === 'number') {
+            return 'Я прыгнул на  ' + a + ' * 100 сантиметров';
         }
-}
+};
         function getArray1MultiplicatingSum(array){
           var distancePath = 0;
           for (var i = 0; i < array.length; i++) {
               distancePath += a[i] * b[i];
-          }
-}
+          }if (Array.isArray(a) && Array.isArray(b)) {
+             'Я прошёл ' + arraySum(a, b) + ' метров';
+           }
+};
           function getArray2MultiplicatingSum(array){
             var numberOfSteps = 0;
             for (var i = 0; i < array.length; i++) {
                 numberOfSteps += a[i];
+              }if (Array.isArray(a)) {
+                 'Я прошёл ' + arraySum(a) + ' метров';
               }
-}
-
-    } else if (typeof a === 'number') {
-        return 'Я прыгнул на  ' + a + ' * 100 сантиметров';
-    } else if (Array.isArray(a) && Array.isArray(b)) {
-       'Я прошёл ' + arraySum(a, b) + ' метров';
-
-    } else if (Array.isArray(a)) {
-       'Я прошёл ' + arraySum(a) + ' метров';
-    }
-}
+  };
