7e11ad65ba37a874ef7d920c1faf5fdb43dde259
Victoria Beregova <gerzhan6@gmail.com>
2016-08-12 21:31:43 +0300

    Модуль 1 задание 2

--- build/js/check.js
+++ build/js/check.js
@@ -2,24 +2,28 @@
 
 function getMessage(a, b) {
     if (typeof a === 'boolean') {
-    return 'Я попал в ' + b + '';
-   }else {
+    if (true) {
+    return 'Я попал в ' + b + '';}
+   } else {
     return 'Я никуда не попал';
-   }else if (typeof a === 'number') {
+   } else if (typeof a === 'number') {
      return 'Я прыгнул на  ' + a + ' * 100 сантиметров';
-   }if (Array.isArray(a) && Array.isArray(b)) {
+   } if (Array.isArray(a) && Array.isArray(b)) {
          'Я прошёл ' + getArrayMultiplicatingSum(a, b) + ' метров';
-    }if (Array.isArray(a)) {
+    } if (Array.isArray(a)) {
          'Я прошёл ' + getArraySum(a) + ' метров';
               }
-    };
+  };
+
+
+
 
     function getArraySum(array){
       var sum = 0;
       for(var i = 0; i < array.length; i++){
       sum += array[i];
       }
-      return(sum);
+      return sum;
        };
 
 
@@ -28,5 +32,5 @@ function getMessage(a, b) {
       for (var i = 0; i < array.length; i++)
       sum += a[i] * b[i];
       }
-      return(sum);
+      return sum;
       };
