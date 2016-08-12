165c151e22a4d2c99a8da0d91b45435504be365c
Victoria Beregova <gerzhan6@gmail.com>
2016-08-12 23:23:47 +0300

    Модуль 1 задание 2

--- build/js/check.js
+++ build/js/check.js
@@ -1,36 +1,39 @@
 'use strict';
 
 function getMessage(a, b) {
-    if (typeof a === 'boolean') {
-    if (true) {
-    return 'Я попал в ' + b + '';}
-   } else {
-    return 'Я никуда не попал';
-   } else if (typeof a === 'number') {
-     return 'Я прыгнул на  ' + a + ' * 100 сантиметров';
-   } if (Array.isArray(a) && Array.isArray(b)) {
-         'Я прошёл ' + getArrayMultiplicatingSum(a, b) + ' метров';
-    } if (Array.isArray(a)) {
-         'Я прошёл ' + getArraySum(a) + ' метров';
-              }
-  };
+ if (typeof a === 'boolean') {
+ if (a === true) {
+ return 'Я попал в ' + b + '';}
+
+ } else {
+ return 'Я никуда не попал';
+
+ } else if (typeof a === 'number') {
+ return 'Я прыгнул на  ' + a + ' * 100 сантиметров';
+
+ }else if (Array.isArray(a) && Array.isArray(b)) {
+ return 'Я прошёл ' + getArrayMultiplicatingSum(a, b) + ' метров';
+
+ }else if (Array.isArray(a)) {
+ return 'Я прошёл ' + getArraySum(a) + ' метров';
+ }
+  }
 
 
 
+function getArraySum(array){
+  var sum = 0;
+  for(var i = 0; i < array.length; i++){
+  sum += array[i];
+  }
+  return sum;
+  }
 
-    function getArraySum(array){
-      var sum = 0;
-      for(var i = 0; i < array.length; i++){
-      sum += array[i];
-      }
-      return sum;
-       };
 
-
-  function getArrayMultiplicatingSum(a, b){
-      var sum = 0;
-      for (var i = 0; i < array.length; i++)
-      sum += a[i] * b[i];
-      }
-      return sum;
-      };
+function getArrayMultiplicatingSum(a, b){
+  var sum = 0;
+  for (var i = 0; i < a.length; i++){
+  sum += a[i] * b[i];
+  }
+  return sum;
+  }
